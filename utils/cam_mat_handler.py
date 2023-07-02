a6338aa04861ca4223396688962982b69396950d
minesaki-ais <minesaki.aislab@gmail.com>
2023-07-02 16:40:59 +0900

    add handler

---
+++ utils/cam_mat_handler.py
@@ -0,0 +1,17 @@
+
+X = 3.636383510923642
+Z = -0.2077758949365421
+THETA = 5.5100281849973
+PITCH = 0
+
+def get_x():
+    return X
+
+def get_z():
+    return Z
+
+def get_theta():
+    return THETA
+
+def get_pitch():
+    return PITCH
