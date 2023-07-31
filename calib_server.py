0c2d4e352fcc3633b7847f087cc57217d7f410b9
minesaki-ais <minesaki.aislab@gmail.com>
2023-07-31 21:21:23 +0900

    fix code

--- calib_server.py
+++ calib_server.py
@@ -30,6 +30,114 @@ END_COUNT = 300
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+class Client:
+    def __init__(self, connection, address):
+        self.connection = connection
+        self.address = address
+        self.data_list = []
+        thread = threading.Thread(target=self.loop)
+        thread.start()
+        self.connection.send('B'.encode(encoding='utf-8'))
+    
+    def loop(self):
+        while(True):
+            try:
+                #クライアント側から受信
+                res = self.connection.recv(4096)
+            except Exception as e:
+                print(e)
+                break
+            try:
+                json_data = json.loads(res.decode('utf-8'))
+                if json_data:
+                    print(json_data)
+                    if 'bx' in json_data and 'bz' in json_data and 'bt' in json_data:
+                        self.bx = json_data['bx']
+                        self.bz = json_data['bz']
+                        self.bt = json_data['bt']
+                    elif 'cx' in json_data and 'cz' in json_data:
+                        cx = json_data['cx']
+                        cz = json_data['cz']
+                        if cz == 0:
+                            self.data_list.append('NoData')
+                        else:
+                            self.data_list.append([cx, cz])
+            except Exception as e:        
+                pass
+    
+    def send_signal(self):
+        self.connection.send('C'.encode(encoding='utf-8'))
+
+    def theta_average(self, t1, t2, t3, t4):
+        theta = np.array([t1, t2, t3, t4])
+        x, y = np.cos(theta), np.sin(theta)
+
+        x_mean = np.mean(x)
+        y_mean = np.mean(y)
+        return np.arctan(y_mean/x_mean)
+
+    def n(self, theta):
+        return (theta+2*math.pi) % (2*math.pi)
+
+    def t(self, value):
+        value = float(value)
+        value = max(1, value)
+        return min(-1, value)
+
+    def get_matrix(self, world_coods):
+        self.X = np.empty((0,2),float)
+        self.W = np.array([])
+        for i in range(len(self.data_list)):
+            if self.data_list[i] == 'NoData' or world_coods[i] == 'NoData':
+                continue
+            self.X = np.append(self.X, np.array([[self.data_list[i][0], self.data_list[i][1]]]), axis=0)
+            self.W = np.append(self.W, np.array([world_coods[i][0], world_coods[i][1]]))
+        self.XX = []
+        for i in range(len(self.X)):
+            self.XX.append([self.X[i][0], self.X[i][1], 1, 0, 0, 0])
+            self.XX.append([0, 0, 0, self.X[i][0], self.X[i][1], 1])
+        self.XX = np.array(self.XX)
+        matrix = np.linalg.inv(self.XX.T.dot(self.XX)).dot(self.XX.T).dot(self.W)
+        dx = matrix[2]
+        dz = matrix[5]
+        #dt = (self.n(math.acos(self.t(matrix[0])))+self.n(math.asin(self.t(-matrix[1])))+self.n(math.asin(self.t(matrix[3])))+self.n(math.acos(self.t(matrix[4]))))/4
+        dt = self.theta_average(math.acos(self.t(matrix[0])), math.asin(self.t(-matrix[1])), math.asin(self.t(matrix[3])), math.acos(self.t(matrix[4])))
+        #dt = self.n(math.acos(self.t(matrix[0])))
+        return dx, dz, dt
+    
+    def get_diff(self, dx, dz, dt):
+        diff_x = 0
+        diff_z = 0
+        diff_dist = 0
+        size = self.X.shape[0]
+        for i in range(self.X.shape[0]):
+            px, pz = self.X[i]
+            temp_x = -dx+px*math.cos(dt)-pz*math.sin(dt)
+            pz = -dz+px*math.sin(dt)+pz*math.cos(dt)
+            px = temp_x
+            wx, wz = self.W[2*i], self.W[2*i-1]
+            diff_x += abs(wx-px)
+            diff_z += abs(wz-pz)
+            diff_dist += abs(math.sqrt((wx-px)**2+(wz-pz)**2))
+        return diff_x/size, diff_z/size, diff_dist/size
+
+    def get_diff_current(self, dx, dz, dt):
+        diff_x = 0
+        diff_z = 0
+        diff_dist = 0
+        size = self.X.shape[0]
+        for i in range(self.X.shape[0]):
+            px, pz = self.X[i]
+            R = np.array([[np.cos(dt), -np.sin(dt), dx], [np.sin(dt), np.cos(dt), dz]], float)
+            M = np.array([[px, pz, 1]])
+            M = R.dot(M.T)
+            px, pz = M[0,0], M[1,0]
+            wx, wz = self.W[2*i], self.W[2*i-1]
+            diff_x += abs(wx-px)
+            diff_z += abs(wz-pz)
+            diff_dist += abs(math.sqrt((wx-px)**2+(wz-pz)**2))
+        return diff_x/size, diff_z/size, diff_dist/size
+
 def loop_handler():
     while True:
         try:
@@ -45,10 +153,11 @@ def loop_handler():
 
 def main():
     global state
-    # sock.bind((HOST, PORT))
-    # sock.listen(10)
-    # thread = threading.Thread(target=loop_handler)
-    # thread.start()
+    global sock
+    sock.bind((HOST, PORT))
+    sock.listen(10)
+    thread = threading.Thread(target=loop_handler)
+    thread.start()
     count = 0
 
     while True:
@@ -86,12 +195,13 @@ def main():
         elif state == 4:
             time.sleep(1)
             base_client = client_list[0]
-            for i in range(len(client_list-1)):
+            for i in range(len(client_list)-1):
                 clt = client_list[i+1]
                 dx, dz, dt = clt.get_matrix(base_client.data_list)
-                diff_x, diff_z, diff_theta = clt.get_diff(dx, dz, dt)
+                diff_x, diff_z, diff_theta = clt.get_diff(-dx, dz, -dt)
                 bdiff_x, bdiff_z, bdiff_theta = clt.get_diff(clt.bx, clt.bz, clt.bt)
-                print(f'result: {diff_x},{diff_z},{diff_theta}')
+                print(f'result: {dx},{dz},{dt}')
+                print(f'before_result: {clt.bx},{clt.bz},{clt.bt}')
                 print(f'before_diff: {bdiff_x},{bdiff_z},{bdiff_theta}')
                 print(f'current_diff: {diff_x},{diff_z},{diff_theta}')
             state = 5
@@ -112,79 +222,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-class Client:
-    def __init__(self, connection, address):
-        self.connection = connection
-        self.address = address
-        self.data_list = []
-        thread = threading.Thread(target=self.loop, args=self)
-        thread.start()
-        self.connection.send('B'.encode(encoding='utf-8'))
-    
-    def loop(self):
-        while(True):
-            try:
-                #クライアント側から受信
-                res = self.connection.recv(4096)
-            except Exception as e:
-                print(e)
-                break
-            try:
-                json_data = json.loads(res.decode('utf-8'))
-                if json_data:
-                    if 'bx' in json_data and 'bz' in json_data and 'bt' in json_data:
-                        self.bx = json_data['bx']
-                        self.bz = json_data['bz']
-                        self.bt = json_data['bt']
-                    elif 'cx' in json_data and 'cz' in json_data:
-                        cx = json_data['cx']
-                        cz = json_data['cz']
-                        if cz == 0:
-                            self.data_list.append('NoData')
-                        else:
-                            self.data_list.append([cx, cz])
-            except Exception as e:        
-                pass
-    
-    def send_signal(self):
-        self.connection.send('C'.encode(encoding='utf-8'))
-
-    def n(self, theta):
-        return (theta+2*math.pi) % (2*math.pi)
-
-    def get_matrix(self, world_coods):
-        self.X = np.empty((0,2),float)
-        self.W = np.array([])
-        for i in range(len(self.data_list)):
-            if self.data_list[i] == 'NoData' or world_coods[i] == 'NoData':
-                continue
-            self.X = np.append(self.X, np.array([[self.data_list[i][0], self.data_list[i][1]]]), axis=0)
-            self.W = np.append(self.W, np.array([world_coods[i][0], world_coods[i][1]]))
-        self.XX = []
-        for i in range(len(self.X)):
-            self.XX.append([self.X[i][0], self.X[i][1], 1, 0, 0, 0])
-            self.XX.append([0, 0, 0, self.X[i][0], self.X[i][1], 1])
-        self.X = np.array(self.XX)
-        matrix = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.W)
-        dx = matrix[2]
-        dz = matrix[5]
-        dt = (self.n(math.acos(matrix[0]))+self.n(math.asin(-matrix[1]))+self.n(math.asin(matrix[3]))+self.n(math.acos(matrix[4])))/4
-        return dx, dz, dt
-    
-    def get_diff(self, dx, dz, dt):
-        diff_x = 0
-        diff_z = 0
-        diff_dist = 0
-        size = self.X.shape[0]
-        for i in range(self.X.shape[0]):
-            px, pz = self.X[i]
-            temp_x = -dx+px*math.cos(dt)-pz*math.sin(dt)
-            pz = -dz+px*math.sin(dt)+pz*math.cos(dt)
-            px = temp_x
-            wx, wz = self.W[i]
-            diff_x += abs(wx-px)
-            diff_z += abs(wz -pz)
-            diff_dist += abs(math.sqrt((wx-px)**2+(wz-pz)**2))
-        return diff_x/size, diff_z/size, diff_dist/size
 
